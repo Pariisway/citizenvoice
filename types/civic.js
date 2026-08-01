105997a69b0bc1e4b347a5dd01998ee8e998dbe5
Whisper+Me <whisper.me.support@gmail.com>
2026-08-01 00:49:55 -0500

    Remove stray compiled output from types/

--- types/civic.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// types/civic.ts
-// Shared types for the "Who Represents Me" feature and general civic data model.
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=civic.js.map
